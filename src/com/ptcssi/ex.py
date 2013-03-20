fbfbb55b72525c2cce92111e6cbbcdf2e8959aec
Jay <yangao1224@gmail.com>
2013-03-20 13:44:42 +0800

    update function

--- src/com/ptcssi/ex.py
+++ src/com/ptcssi/ex.py
@@ -5,18 +5,18 @@ Created on 2013-3-13
 '''
 
 name = 'Jay'
-country_dict = {'dan':'us', 'tim':'us','rupa':'in','shrek':'cn','mirek':'pl','jay':'cn'}
+country_dict = {'dan':'us', 'tim':'us', 'rupa':'in', 'shrek':'cn', 'mirek':'pl', 'jay':'cn'}
 
 def square(n):
-    try:print int(n**2)
-    except:print'oops!' 
+    try:return int(n ** 2)
+    except:return 'oops!' 
     
 def get_country_by_member_name(first_name):
     try: return country_dict[str(first_name).lower()]
     except: return 'Sorry, not found!'
 
-square(3)
-square(2.5)
-square(name)
+print square(3)
+print square(2.5)
+print square(name)
 
 print get_country_by_member_name('jAy')
